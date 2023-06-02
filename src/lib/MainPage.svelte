dfb2b0397aedc93b90957e9107d78395845671b2
Marc Hyeler <birdpump@gmail.com>
2023-06-02 19:41:59 +0000

    fix

---
+++ src/lib/MainPage.svelte
@@ -0,0 +1,62 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+
+    function sayHello() {
+        dispatch('switch');
+    }
+
+</script>
+
+<style>
+	.main{
+		width: 100%;
+		height: 100%;
+		background-color: #282C34;
+		display: flex;
+		align-items: center;
+		justify-content: space-evenly;
+		flex-direction: column;
+	}
+	
+	.inner{
+/* 		background-color: black; */
+	}
+	
+	.text{
+		font-size: 30px;
+		font-family: Roboto, sans-serif;
+    color: white;
+/* 		margin-bottom: 10px; */
+	}
+	
+	.start{
+		padding: 15px;
+		text-align: center;
+		background-color: #a341e4;
+		border-radius: 5px;
+		cursor: pointer;
+		color: white;
+		font-family: Roboto, sans-serif;
+		font-size: 20px
+/* 		font-weight: bold; */
+			
+	}
+	
+	.start:hover{
+		filter: drop-shadow(0 0 6px #a341e4)
+	}
+	
+	.instruct{
+		width: 50%;
+		text-align: center;
+		color: white;
+	}
+</style>
+<div class="main">
+		<div class="text">Fast Captcha</div>
+		
+		<div class="instruct">Your objective is to complete the Recaptchas as quickly and accurately as possible. Be careful! Selecting the wrong objects will result in a score penalty. The faster and more accurately you complete the Recaptchas, the higher your score will be.</div>
+	<div class="start" on:click={sayHello}>Start Game</div>
+</div>
